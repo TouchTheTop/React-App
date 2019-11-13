83b46fe3882c59ccc6de90069d37668dc430840f
Christine <1204356932@qq.com>
2019-11-13 18:09:26 +0800

    :pencil:

--- src/pages/Button/index.js
+++ src/pages/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Button extends React.Component {
   render() {
-    return <div className="page-button">button</div>
+    return <div className="page-button app-wrap">button</div>
   }
 }
 
